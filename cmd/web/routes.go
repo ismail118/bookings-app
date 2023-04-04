46ad92465a733c88d3eda30c90939f7ca86da2e7
ismail118 <ismailalfiyasin6@gmail.com>
2023-04-04 20:05:32 +0700

    push update

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -17,6 +17,15 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/room-one", handlers.Repo.RoomOne)
+	mux.Get("/room-two", handlers.Repo.RoomTwo)
+
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Post("/search-availability-json", handlers.Repo.PostAvailabilityJSON)
+
+	mux.Get("/contact", handlers.Repo.Contact)
+	mux.Get("/make-reservation", handlers.Repo.MakeReservation)
 
 	fileServer := http.FileServer(http.Dir("static"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/ismail118/bookings-app/pkg/config"
 	"github.com/ismail118/bookings-app/pkg/models"
 	"github.com/ismail118/bookings-app/pkg/render"
+	"log"
 	"net/http"
 )
 
@@ -32,7 +35,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
+	render.RenderTemplate(w, r, "home.page.gohtml", &models.TemplateData{})
 }
 
 // About is the handler for the about page
@@ -45,7 +48,63 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	stringMap["remote_ip"] = remoteIP
 
 	// send data to the template
-	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{
+	render.RenderTemplate(w, r, "about.page.gohtml", &models.TemplateData{
 		StringMap: stringMap,
 	})
+}
+
+// RoomOne renders the room one page
+func (m *Repository) RoomOne(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "room-one.page.gohtml", &models.TemplateData{})
+}
+
+// RoomTwo renders the room two page
+func (m *Repository) RoomTwo(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "room-two.page.gohtml", &models.TemplateData{})
+}
+
+// Availability renders the search availability page
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "search-availability.page.gohtml", &models.TemplateData{})
+}
+
+// PostAvailability renders the search availability page
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.PostForm.Get("start")
+	end := r.PostForm.Get("end")
+
+	_, err := w.Write([]byte(fmt.Sprintf("%s %s", start, end)))
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+type jsonResponse struct {
+	Ok      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// PostAvailabilityJSON handle renders for availability and send JSON response
+func (m *Repository) PostAvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	res := jsonResponse{
+		Ok:      true,
+		Message: "Available!",
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Set("Content-Type", "application/json")
+	err := json.NewEncoder(w).Encode(res)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+// Contact renders the contact page
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.gohtml", &models.TemplateData{})
+}
+
+// Contact renders the contact page
+func (m *Repository) MakeReservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "make-reservation.page.gohtml", &models.TemplateData{})
 }
