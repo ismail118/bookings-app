731d00ec34b8c57a7c660abbe099d4938e388f4a
ismail118 <ismailalfiyasin6@gmail.com>
2023-04-08 12:29:11 +0700

    add test for forms package

---
+++ internal/forms/error_test.go
@@ -0,0 +1,23 @@
+package forms
+
+import "testing"
+
+func TestErrors_Add(t *testing.T) {
+	errs := errors{}
+	errs.Add("e", "e")
+
+	if len(errs) == 0 {
+		t.Error("len should not 0")
+	}
+}
+
+func TestErrors_Get(t *testing.T) {
+	errs := errors{
+		"e": []string{"e"},
+	}
+	e := errs.Get("e")
+
+	if e == "" {
+		t.Error("failed get")
+	}
+}
