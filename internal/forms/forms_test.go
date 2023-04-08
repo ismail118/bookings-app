d0e28ed98b1ffc645e4f67843892415bc2b8418c
ismail118 <ismailalfiyasin6@gmail.com>
2023-04-08 12:24:02 +0700

    add test for forms package

---
+++ internal/forms/forms_test.go
@@ -0,0 +1,135 @@
+package forms
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+)
+
+func TestForm_Valid(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	f := New(r.Form)
+	isValid := f.Valid()
+	if !isValid {
+		t.Error("got invalid when should valid")
+	}
+}
+
+func TestForm_Required(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	f := New(r.Form)
+
+	f.Required("a", "b")
+
+	isValid := f.Valid()
+	if isValid {
+		t.Error("got valid when should invalid")
+	}
+
+	r = httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	f = New(r.Form)
+
+	formData := url.Values{}
+	formData.Add("a", "a")
+	formData.Add("b", "b")
+	f.Data = formData
+
+	f.Required("a", "b")
+
+	isValid = f.Valid()
+	if !isValid {
+		t.Error("got invalid when should valid")
+	}
+}
+
+func TestForm_Has(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	formData := url.Values{}
+	formData.Add("a", "a")
+	r.Form = formData
+
+	f := New(r.Form)
+
+	f.Has("a", r)
+
+	isValid := f.Valid()
+	if !isValid {
+		t.Error("got invalid when should valid")
+	}
+
+	r = httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	f = New(r.Form)
+
+	f.Has("a", r)
+
+	isValid = f.Valid()
+	if isValid {
+		t.Error("got valid when should invalid")
+	}
+}
+
+func TestForm_MinLength(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	formData := url.Values{}
+	formData.Add("a", "a")
+	r.Form = formData
+
+	f := New(r.Form)
+
+	f.MinLength("a", 1, r)
+
+	isValid := f.Valid()
+	if !isValid {
+		t.Error("got invalid when should valid")
+	}
+
+	r = httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	f = New(r.Form)
+
+	f.MinLength("a", 1, r)
+
+	isValid = f.Valid()
+	if isValid {
+		t.Error("got valid when should invalid")
+	}
+}
+
+func TestForm_IsEmail(t *testing.T) {
+	r := httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	formData := url.Values{}
+	formData.Add("a", "a@email.com")
+	r.Form = formData
+
+	f := New(r.Form)
+
+	f.IsEmail("a")
+
+	isValid := f.Valid()
+	if !isValid {
+		t.Error("got invalid when should valid")
+	}
+
+	r = httptest.NewRequest(http.MethodPost, "/whatever", nil)
+
+	formData = url.Values{}
+	formData.Add("a", "a")
+	r.Form = formData
+
+	f = New(r.Form)
+
+	f.IsEmail("a")
+
+	isValid = f.Valid()
+	if isValid {
+		t.Error("got valid when should invalid")
+	}
+}
