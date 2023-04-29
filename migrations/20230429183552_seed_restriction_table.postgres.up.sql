5f6e9b91039ce709a8e3b34c891319c6da1a3f38
ismail118 <ismailalfiyasin6@gmail.com>
2023-04-30 01:37:14 +0700

    update migration

---
+++ migrations/20230429183552_seed_restriction_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (id, restriction_name, created_at, updated_at) VALUES
+(1, 'Reservation', '2023-04-22 09:27:18.382536', '2023-04-22 09:27:18.382536'),
+(2, 'Owner Block', '2023-04-24 19:05:32.000000', '2023-04-24 19:05:48.000000');
