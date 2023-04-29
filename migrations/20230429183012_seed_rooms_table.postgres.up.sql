a4a261e3b8ca697fdda6ab31d2da409cf97069da
ismail118 <ismailalfiyasin6@gmail.com>
2023-04-30 01:35:07 +0700

    update migration

---
+++ migrations/20230429183012_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.rooms (id, room_name, created_at, updated_at) VALUES
+(1, 'room_one', '2023-04-22 00:00:00.000000', '2023-04-22 00:00:00.000000'),
+(2, 'room_two', '2023-04-24 19:46:10.000000', '2023-04-24 19:46:13.000000');
