be1c5a85be62761a8dcbfc886d0c182f3d130fcc
ismail118 <ismailalfiyasin6@gmail.com>
2023-05-19 02:59:05 +0700

    update test add flags

--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -1,8 +1,12 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestRun(t *testing.T) {
+	os.Args = []string{"test", "-dbname", "bookings_app", "-dbuser", "postgres", "-dbpass", "postgres", "-cache", "false", "-production", "false"}
 	_, err := run()
 	if err != nil {
 		t.Error("failed run()")
